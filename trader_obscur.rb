25f1f4406d1f52e4d7ecb723e6d57224f15420fe
hguerni <hava3@hotmail.fr>
2018-10-11 18:15:16 +0200

    Affichage upgraded

--- trader_obscur.rb
+++ trader_obscur.rb
@@ -2,15 +2,20 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_the_currency(market_url)
-	doc = Nokogiri::HTML(open(market_url))
+	page = Nokogiri::HTML(open(market_url))
 	liste = Hash.new
-	i = 0
-	while i < doc.css("a.price").size
-		liste[doc.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div[2]/table/tbody/tr[#{i}]/td[2]/a").text] = doc.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div[2]/table/tbody/tr[#{i}]/td[5]/a").text
+	i = 1 ; j = 0
+	while i <= page.css("a.price").size
+		liste[page.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div[2]/table/tbody/tr[#{i}]/td[2]/a").text] = page.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div[2]/table/tbody/tr[#{i}]/td[5]/a").text
+		system 'clear'
+		puts "              ...LOADING, WAIT PLEASE...                \n" + ("|"*j+" "*(58-j)+"|")
+		if j == 58
+			j = 0 
+		end
 		i += 1
+		j += 1
 	end
 	print liste
 end
 
 get_the_currency("https://coinmarketcap.com/all/views/all/")
-
