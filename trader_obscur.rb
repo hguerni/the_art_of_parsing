09e5cb5cf42548902e037aec4f30c1c3e9d95c22
hguerni <hava3@hotmail.fr>
2018-10-11 19:08:53 +0200

    Orthographe corrigé

--- trader_obscur.rb
+++ trader_obscur.rb
@@ -8,7 +8,7 @@ def get_the_currency(market_url)
 	while i <= page.css("a.price").size
 		liste[page.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div[2]/table/tbody/tr[#{i}]/td[2]/a").text] = page.xpath("/html/body/div[2]/div/div[1]/div[2]/div[3]/div[2]/table/tbody/tr[#{i}]/td[5]/a").text
 		system 'clear'
-		puts "              ...LOADING, WAIT PLEASE...                \n" + ("|"*j+" "*(58-j)+"|")
+		puts "              ...LOADING, PLEASE WAIT...                \n" + ("|"*j+" "*(58-j)+"|")
 		if j == 58
 			j = 0 
 		end
